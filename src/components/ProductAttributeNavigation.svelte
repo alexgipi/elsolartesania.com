d11868291de5b8811681c58b6faec0949d035d3f
alexgipi <53187869+alexgipi@users.noreply.github.com>
2024-08-29 21:35:59 +0200

    Start new component for navigate on product attribute options

---
+++ src/components/ProductAttributeNavigation.svelte
@@ -0,0 +1,40 @@
+<footer style="
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    bottom: 0;
+    left: -8px;
+    width: calc(106%);
+    padding: 0;
+    align-items: center;
+">
+    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10" data-astro-cid-qi4rb7oa="" style="
+    transform: rotate(180deg);
+    width: 50px;
+    height: 50px;
+    padding: 16px;
+    background: #ffffff;
+    outline: 1px solid #000000;
+"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" data-astro-cid-qi4rb7oa=""></path></svg>
+    <span style="
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 1rem;
+    background: #fff;
+    padding: 12px 20px;
+    outline: 1px solid #000000;
+    color: #000000;
+    translate: 0 47px;
+    flex: auto;
+    text-align: center;
+    position: absolute;
+    width: 100%;
+">Cajita Arbol psicodelico</span>
+    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10" data-astro-cid-qi4rb7oa="" style="
+    width: 50px;
+    height: 50px;
+    padding: 16px;
+    background: #fff;
+    outline: 1px solid #000000;
+"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" data-astro-cid-qi4rb7oa=""></path></svg>
+</footer>
