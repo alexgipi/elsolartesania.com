f625842a81e47416d3232e463835d269a1266c86
alexgipi <53187869+alexgipi@users.noreply.github.com>
2026-06-11 10:49:51 +0200

    feat: implement new user authentication flow

--- src/components/StoreMessage.svelte
+++ src/components/StoreMessage.svelte
@@ -14,7 +14,7 @@
             🎪 Temporalmente sin envíos hasta el día 4 de Marzo. Si no puedes esperar nos puedes encontrar en la Feria de Vitoria. Agradecemos tu comprensión. 🌞
         </p> -->
         <p class="text-sm leading-6 text-white">
-            🎪 Temporalmente sin envíos hasta el día 01 de Abril. Si realizas una compra ten en cuenta que se procesará a partir de esta fecha. Agradecemos tu comprensión. 🌞
+            🎪 Temporalmente sin envíos hasta el día 23/06/2026. Si realizas una compra ten en cuenta que se procesará a partir de esta fecha. Agradecemos tu comprensión. 🌞
         </p>
         </div>
         <div class="flex flex-1 justify-end">
